096cbd247d8fc8c7f7d6d117e7bb84834145664b
Jacob Norwood <jacobnorwood92@gmail.com>
2021-11-14 18:11:27 -0600

    style

--- src/ui/components/App.tsx
+++ src/ui/components/App.tsx
@@ -1,20 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Settings from "../scenes/Settings";
 import MainMenu from "../scenes/MainMenu";
 import Game from "../scenes/Game";
-import { store } from "../../system/store"
-import { useAppSelector, useAppDispatch } from "../../system/hooks";
+import { useAppSelector } from "../../system/hooks";
 
 const App = () => {
   const scene = useAppSelector(state => state.scenes.activeScene);
 
+  const styles = {
+    wrapper: {
+      display: "flex",
+      justifyContent: "flex-end",
+      width: "100%",
+      position: "absolute" as 'absolute',
+    }
+  }
+
   return (
-    <div style={{ 
-      display: "flex", 
-      justifyContent: "flex-end", 
-      width: "100%", 
-      position: "absolute" 
-      }}>
+    <div style={styles.wrapper}>
       {scene === "main" && <MainMenu />}
       {scene === "settings" && <Settings />}
       {scene === "game" && <Game />}
